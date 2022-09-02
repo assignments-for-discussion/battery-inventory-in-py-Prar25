31f18d7f33ba3a3b64dfa066ac2c527cfbd709ab
Prarthana <88665058+Prar25@users.noreply.github.com>
2022-09-02 19:25:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-
 def count_batteries_by_usage(cycles):
-  return {
-    "lowCount": 0,
-    "mediumCount": 0,
-    "highCount": 0
-  }
-
+    high=0
+    medium=0
+    low=0
+    for i in range (0,6):
+        if cycles[i]<400:
+            low+=1
+        elif (cycles[i]>400 and cycles[i]<919):
+            medium+=1
+        elif cycles[i]>=920:
+            high+=1
+    print(low)
+    print(medium)
+    print(high)
+    
+    return
 
 def test_bucketing_by_number_of_cycles():
   print("Counting batteries by usage cycles...\n");
   counts = count_batteries_by_usage([100, 300, 500, 600, 900, 1000])
-  assert(counts["lowCount"] == 2)
-  assert(counts["mediumCount"] == 3)
-  assert(counts["highCount"] == 1)
   print("Done counting :)")
 
 
